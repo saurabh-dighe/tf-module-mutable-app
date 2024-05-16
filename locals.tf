942983d3dec117e3d54330f1223867e0102ea960
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-16 14:57:11 +0530

    Auto push

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  INSTANCE_COUNT = "${var.OD_INSTANCE_COUNT} + ${var.SPOT_INSTANCE_COUNT}"
+  INSTANCE_COUNT = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
   INSTANCE_IDS   = concat(aws_spot_instance_request.spot-app.*.spot_instance_id, aws_instance.OD-app.*.id)
 }
