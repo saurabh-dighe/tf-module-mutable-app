739b4d75ea1f4fafbba0b7f412d515df4936fd9d
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-16 23:30:48 +0530

    Auto push

--- app.tf
+++ app.tf
@@ -1,17 +1,17 @@
-# resource "null_resource" "remote_provisioner" {
-#   # Establishes connection to be used by all
-#   connection {
-#     type     = "ssh"
-#     user     = local.SSH_USERNAME
-#     password = local.SSH_PASSWORD
-#     host     = aws_spot_instance_request.rabbitmq.private_ip
-#   }
+resource "null_resource" "remote_provisioner" {
+  # Establishes connection to be used by all
+  connection {
+    type     = "ssh"
+    user     = local.SSH_USERNAME
+    password = local.SSH_PASSWORD
+    host     = aws_spot_instance_request.rabbitmq.private_ip
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "ansible-pull -U https://github.com/saurabh-dighe/Ansible.git -e ENV=dev -e COMPONENT=rabbitmq roboshop-pull.yml"
-#     ]
-#   }
-# }
+  provisioner "remote-exec" {
+    inline = [
+      "ansible-pull -U https://github.com/saurabh-dighe/Ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} roboshop-pull.yml"
+    ]
+  }
+}
 
 
--- locals.tf
+++ locals.tf
@@ -4,7 +4,3 @@ locals {
   SSH_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["SSH_USERNAME"]
   SSH_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["SSH_PASSWORD"]
 }
-
-output "name" {
-    value = local.SSH_PASSWORD
-}
