59c77ec270e7a59033e590a65e2175a39c598c73
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-16 23:09:42 +0530

    Auto push

--- ec2.tf
+++ ec2.tf
@@ -29,22 +29,3 @@ resource "aws_ec2_tag" "instance-tag" {
 
 
 
-# Provisions a OD instance
-
-# resource "null_resource" "remote_provisioner" {
-#   # Establishes connection to be used by all
-#   connection {
-#     type     = "ssh"
-#     user     = local.SSH_USERNAME
-#     password = local.SSH_PASSWORD
-#     host     = aws_spot_instance_request.rabbitmq.private_ip
-#   }
-
-#   provisioner "remote-exec" {
-#     inline = [
-#       "ansible-pull -U https://github.com/saurabh-dighe/Ansible.git -e ENV=dev -e COMPONENT=rabbitmq roboshop-pull.yml"
-#     ]
-#   }
-# }
-
-
