13c92faec4b59bf151aa06c496bf367ba2aab678
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-16 23:13:22 +0530

    Auto push

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   INSTANCE_COUNT = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
   INSTANCE_IDS   = concat(aws_spot_instance_request.spot-app.*.spot_instance_id, aws_instance.OD-app.*.id)
-  SSH_USERNAME   = data.aws_secretsmanager_secret_version.secret_version.secret_string[4]
-  SSH_PASSWORD   = data.aws_secretsmanager_secret_version.secret_version.secret_string[5]
+  SSH_USERNAME   = data.aws_secretsmanager_secret_version.secret_version.secret_string["SSH_USERNAME"]
+  SSH_PASSWORD   = data.aws_secretsmanager_secret_version.secret_version.secret_string["SSH_PASSWORD"]
 }
 
 output "name" {
