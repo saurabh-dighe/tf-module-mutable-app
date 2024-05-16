7340b5079d877de62e3a46ec0c6422f10a4a21f9
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-16 15:49:22 +0530

    Auto push

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,24 @@
-# Provisions a spot instance
-resource "aws_spot_instance_request" "spot-app" {
-  count                       = var.SPOT_INSTANCE_COUNT
-  ami                         = data.aws_ami.ansible_ami.id
-  instance_type               = var.SPOT_INSTANCE_TYPE
-  subnet_id                   = var.INTERNAL? element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID, count.index) : element(data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID, count.index)
-  vpc_security_group_ids      = var.INTERNAL? element([aws_security_group.allow_private.*.id], count.index) : element([aws_security_group.allow_public.*.id], count.index)
-  associate_public_ip_address = var.INTERNAL? false : true
-  wait_for_fulfillment        = true
-  iam_instance_profile        = "EC2-admin"
-}
+# # Provisions a spot instance
+# resource "aws_spot_instance_request" "spot-app" {
+#   count                       = var.SPOT_INSTANCE_COUNT
+#   ami                         = data.aws_ami.ansible_ami.id
+#   instance_type               = var.SPOT_INSTANCE_TYPE
+#   subnet_id                   = var.INTERNAL? element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID, count.index) : element(data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID, count.index)
+#   vpc_security_group_ids      = var.INTERNAL? element([aws_security_group.allow_private.*.id], count.index) : element([aws_security_group.allow_public.*.id], count.index)
+#   associate_public_ip_address = var.INTERNAL? false : true
+#   wait_for_fulfillment        = true
+#   iam_instance_profile        = "EC2-admin"
+# }
 
-resource "aws_instance" "OD-app"{
-  count                       = var.OD_INSTANCE_COUNT
-  ami                         = data.aws_ami.ansible_ami.id
-  instance_type               = var.OD_INSTANCE_TYPE
-  subnet_id                   = var.INTERNAL? element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID, count.index) : element(data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID, count.index)
-  vpc_security_group_ids      = var.INTERNAL? element([aws_security_group.allow_private.*.id], count.index) : element([aws_security_group.allow_public.*.id], count.index)
-  associate_public_ip_address = var.INTERNAL? false : true
-  iam_instance_profile        = "EC2-admin"
-}
+# resource "aws_instance" "OD-app"{
+#   count                       = var.OD_INSTANCE_COUNT
+#   ami                         = data.aws_ami.ansible_ami.id
+#   instance_type               = var.OD_INSTANCE_TYPE
+#   subnet_id                   = var.INTERNAL? element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID, count.index) : element(data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID, count.index)
+#   vpc_security_group_ids      = var.INTERNAL? element([aws_security_group.allow_private.*.id], count.index) : element([aws_security_group.allow_public.*.id], count.index)
+#   associate_public_ip_address = var.INTERNAL? false : true
+#   iam_instance_profile        = "EC2-admin"
+# }
 
 resource "aws_ec2_tag" "spot-app" {
   count                       = local.INSTANCE_COUNT
