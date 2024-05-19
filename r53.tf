b9564b84376c0988783a2edd8ac8f1c86b588d8d
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-19 22:43:07 +0530

    Auto push

--- r53.tf
+++ r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "record" {
   zone_id = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTEDZONE_ID
-  name    = "${COMPONENT}-${var.ENV}"
+  name    = "${var.COMPONENT}-${var.ENV}"
   type    = "A"
   ttl     = 10
   records = var.INTERNAL ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
