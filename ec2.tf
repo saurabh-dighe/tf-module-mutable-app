cc5a392f3f0c4e66c33efd1f9c49143d13cd1cbd
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-20 14:38:21 +0530

    Auto push

--- ec2.tf
+++ ec2.tf
@@ -1,26 +1,29 @@
 # Provisions a spot instance
 resource "aws_spot_instance_request" "spot-app" {
   count                       = var.SPOT_INSTANCE_COUNT
+
   ami                         = data.aws_ami.ansible_ami.id
   instance_type               = var.SPOT_INSTANCE_TYPE
   subnet_id                   = var.INTERNAL? element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID, count.index) : element(data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID, count.index)
   vpc_security_group_ids      = var.INTERNAL? element([aws_security_group.allow_private.*.id], count.index) : element([aws_security_group.allow_public.*.id], count.index)
-  associate_public_ip_address = var.INTERNAL? false : true
+  # associate_public_ip_address = var.INTERNAL? false : true
   wait_for_fulfillment        = true
   iam_instance_profile        = "EC2-admin"
 }
 
 resource "aws_instance" "OD-app"{
   count                       = var.OD_INSTANCE_COUNT
+
   ami                         = data.aws_ami.ansible_ami.id
   instance_type               = var.OD_INSTANCE_TYPE
   subnet_id                   = var.INTERNAL? element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID, count.index) : element(data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID, count.index)
   vpc_security_group_ids      = var.INTERNAL? element([aws_security_group.allow_private.*.id], count.index) : element([aws_security_group.allow_public.*.id], count.index)
-  associate_public_ip_address = var.INTERNAL? false : true
+  # associate_public_ip_address = var.INTERNAL? false : true
   iam_instance_profile        = "EC2-admin"
 }
 resource "aws_ec2_tag" "instance-tag" {
   count                       = local.INSTANCE_COUNT
+  
   resource_id                 = element(local.INSTANCE_IDS, count.index)
   key                         = "Name"
   value                       = "roboshop-${var.ENV}-${var.COMPONENT}-${count.index+1}"
--- target-group.tf
+++ target-group.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_target_group" "app" {
-  name     = "${var.COMPONENT}-${var.ENV}-tg"
+  name     = "${var.COMPONENT}-${var.ENV}-target-group"
   port     = var.APP_PORT
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
@@ -8,16 +8,15 @@ resource "aws_lb_target_group" "app" {
     enabled             = true             # Enable health checks (default)
     interval            = 10               # Check every 30 seconds
     path                = "/health"        # Path to check (e.g., root for web servers)   
-    protocol            = "HTTP"           # Match the target group's protocol
     healthy_threshold   = 3                # 3 successful checks mark target healthy
     unhealthy_threshold = 2                # 2 failed checks mark target unhealthy
-    timeout             = 5                # 5 seconds timeout for each check
-    matcher             = "200"            # Successful response codes (HTTP status 200)
+    timeout             = 5                # 5 seconds timeout for each check           # Successful response codes (HTTP status 200)
   }
 }
 
 resource "aws_lb_target_group_attachment" "app" {
   count            = local.INSTANCE_COUNT
+  
   target_group_arn = aws_lb_target_group.app.arn
   target_id        = element(local.INSTANCE_IDS, count.index)
   port             = var.APP_PORT
