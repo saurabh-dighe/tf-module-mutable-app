0b7e25afcd2901c28992408e77521783f577f393
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-26 13:51:42 +0530

    Auto push

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "saurabh-bucket-tf"
+    bucket = "saurabh-bucket-tf1"
     key    = "dev/tf-vpc/teraform.tfstate"
     region = "us-east-1"
   }
@@ -9,7 +9,7 @@ data "terraform_remote_state" "vpc" {
 data "terraform_remote_state" "alb" {
   backend = "s3"
   config = {
-    bucket = "saurabh-bucket-tf"
+    bucket = "saurabh-bucket-tf1"
     key    = "dev/tf-alb/teraform.tfstate"
     region = "us-east-1"
   }
@@ -17,7 +17,7 @@ data "terraform_remote_state" "alb" {
 data "terraform_remote_state" "db" {
   backend = "s3"
   config = {
-    bucket = "saurabh-bucket-tf"
+    bucket = "saurabh-bucket-tf1"
     key    = "dev/tf-databases/teraform.tfstate"
     region = "us-east-1"
   }
@@ -25,7 +25,7 @@ data "terraform_remote_state" "db" {
 data "aws_ami" "ansible_ami" {
   most_recent      = true
   name_regex       = "Ansible-AMI"
-  owners           = ["058264375008"]
+  owners           = ["767397841012"]
 }
 
 data "aws_secretsmanager_secret" "roboshop_secrets" {
