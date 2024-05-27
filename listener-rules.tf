e49f0f7f14ae68a454c9e75e1156eaaf88f8b2aa
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-27 12:03:30 +0530

    Auto push

--- listener-rules.tf
+++ listener-rules.tf
@@ -4,7 +4,7 @@ resource "random_integer" "priority" {
   max = 500
 }
 #  private Listener Rule
-resource "aws_lb_listener_rule" "prv_app_rule" {
+resource "aws_lb_listener_rule" "pvt_app_rule" {
   count        = var.INTERNAL  ? 1 : 0 
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN[0]
   priority     = random_integer.priority.result
