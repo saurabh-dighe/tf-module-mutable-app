16651464d36b5bd819d5094a1035d74a13357635
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-29 13:50:56 +0530

    Auto push

--- sg.tf
+++ sg.tf
@@ -18,6 +18,13 @@ resource "aws_security_group" "allow_public" {
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
+  ingress {
+    from_port        = 9100
+    to_port          = 9100
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -40,6 +47,12 @@ resource "aws_security_group" "allow_private" {
   ingress {
     from_port        = var.APP_PORT
     to_port          = var.APP_PORT
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+  ingress {
+    from_port        = 9100
+    to_port          = 9100
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
